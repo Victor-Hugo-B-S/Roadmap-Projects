59b708e41633582d0f27d435688796d476be62d9
Victor Hugo <victorhugobarriossalas@gmail.com>
2026-05-07 13:35:43 -0600

    feat: create tasks file if it does not exist

---
+++ backend/beginner/task-tracker/src/index.js
@@ -0,0 +1,14 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const dataDir = path.join(process.cwd(), "data");
+const filePath = path.join(dataDir, "tasks.json");
+
+await fs.mkdir(dataDir, { recursive: true });
+
+try {
+  await fs.writeFile(filePath, "[]", { flag: "wx" });
+  console.log("Tasks file created");
+} catch (err) {
+  if (err.code !== "EEXIST") throw err;
+}
