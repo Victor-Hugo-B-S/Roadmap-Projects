bb89710b5b329726e07e5653c25b56417565bda6
Victor Hugo <victorhugobarriossalas@gmail.com>
2026-05-07 19:52:54 -0600

    feat: add command

--- backend/beginner/task-tracker/src/index.js
+++ backend/beginner/task-tracker/src/index.js
@@ -20,7 +20,43 @@ async function saveTasks() {
   try {
     await fs.writeFile(filePath, JSON.stringify(tasks), { flag: "w" });
     console.log("succesfully saved");
+    return true;
   } catch (err) {
     throw err;
   }
+}
+
+const args = process.argv.slice(2);
+const command = args[0] ? args[0] : false;
+
+switch (command) {
+  case "add":
+    if (argNotExist(1, "Description")) break;
+    const description = args[1].trim();
+
+    const id = tasks.length > 0 ? Math.max(...tasks.map((t) => t.id)) + 1 : 0;
+
+    tasks.push({
+      id,
+      description,
+      status: "todo",
+      createdAt: Date.now(),
+      updatedAt: null,
+    });
+
+    await saveTasks();
+    console.log("Saved with id: " + id);
+    break;
+
+  default:
+    console.log(`Command "${command}" does not exist`);
+    break;
+}
+
+function argNotExist(i, value) {
+  if (!args[i]) {
+    console.log(`${value} is missing`);
+    return true;
+  }
+  return false;
 }
