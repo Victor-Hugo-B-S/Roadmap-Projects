36f4ba2feb95c50a2fdae3f984cea1024c4b59cf
Victor Hugo <victorhugobarriossalas@gmail.com>
2026-05-13 17:24:47 -0600

    feat: fetch user events from GitHub API

--- backend/beginner/github-user-activity/index.js
+++ backend/beginner/github-user-activity/index.js
@@ -2,3 +2,13 @@
 
 const user = process.argv[2] ?? "midudev";
 console.log("user: ", user);
+
+try {
+  const response = await fetch(`https://api.github.com/users/${user}/events`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const data = await response.json();
+} catch (e) {
+  console.log("---error: ", e);
+}
