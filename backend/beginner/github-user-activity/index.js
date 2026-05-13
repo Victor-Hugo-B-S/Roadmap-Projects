8c41ff1e508c92121e4ae9e674fdb6bb186de72c
Victor Hugo <victorhugobarriossalas@gmail.com>
2026-05-13 17:29:11 -0600

    feat: group events by repository and event type

--- backend/beginner/github-user-activity/index.js
+++ backend/beginner/github-user-activity/index.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
-const user = process.argv[2] ?? "midudev";
-console.log("user: ", user);
-
 try {
   const response = await fetch(`https://api.github.com/users/${user}/events`);
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
   const data = await response.json();
+  const summary = data.reduce((acc, event) => {
+    const repo = event.repo.name;
+    const type = event.type;
+
+    if (!acc[repo]) {
+      acc[repo] = {};
+    }
+
+    acc[repo][type] = (acc[repo][type] ?? 0) + 1;
+    return acc;
+  }, {});
 } catch (e) {
   console.log("---error: ", e);
 }
